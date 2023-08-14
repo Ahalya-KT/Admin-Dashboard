5513157c12c8599906f10aaa6930986341066e01
Ahalya-KT <ahalyakt002@gmail.com>
2023-08-14 11:56:52 +0530

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { LeadComponent } from './lead/lead.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  {path:'',redirectTo:'dashboard',pathMatch:'full'},
+  {path:'employee',component:EmployeeComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'leads',component:LeadComponent},
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+    @Output() onMenuBtnClick=new EventEmitter
+      menuButtonClicked(){
+         
+    //to occur an userDefined event
+    this.onMenuBtnClick.emit()
+    }
+}
